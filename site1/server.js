393e5ec822001fbe8ddd745d4440a17cc262564d
m-sergey <m-sergey@localhost.local>
2023-06-29 11:26:02 +0300

    Added secure cookie

--- site1/server.js
+++ site1/server.js
@@ -5,8 +5,36 @@ var express = require('express');
 var app = module.exports = express()
 var cookieParser = require('cookie-parser');
 var cors = require('cors');
+const fs = require("fs");
+const https = require("https");
 
-app.use(cors());
+const options = {
+  key: fs.readFileSync('security/key.pem'),
+  cert: fs.readFileSync('security/cert.pem')
+};
+
+app.use(cors({credentials: true, origin: 'http://localhost:3001'}));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://site2.local:3001');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Set-Cookie');
+    res.setHeader('Access-Control-Allow-Headers', 'Set-Cookie');
+
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(cookieParser('my secret here'));
 
 app.get('/', function(req, res){
@@ -14,17 +42,32 @@ app.get('/', function(req, res){
 });
 
 app.get('/exchange', function(req, res){
-    res.cookie('remember', 1, {});
+    res.cookie('remember', 1, {
+      domain: "idp.local",
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: 'None', 
+      secure: true
+    });
     res.send('Exchange');
   });
 
 app.get('/forget', function(req, res){
-    res.clearCookie('remember');
+    // res.clearCookie('remember');
+    res.cookie('remember', 0, {
+      domain: "idp.local",
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: 'None', 
+      secure: true
+    });
+
     res.send('Cookie was cleaned');
 });
 
 app.get('/restricted', function(req, res){
-    if (req.cookies.remember) {
+    console.log('req.cookies.remember=' + req.cookies.remember);
+    if (req.cookies.remember === "1") {
         res.send('Restricted content');
     } else {
       res.send(401, 'Access denied');
@@ -32,6 +75,6 @@ app.get('/restricted', function(req, res){
 });
 
 if (!module.parent) {
-  app.listen(3000);
+  https.createServer(options, app).listen(3000);
   console.log('Express started on port 3000');
 }
--- site2/static/css/index.js
+++ site2/static/css/index.js
@@ -1,16 +1,30 @@
 function openSite1() {
 
-    if($("#isExchange").is(":checked")) {
-        var jqxhr = $.get( $("#url").val() + "/exchange", function(data) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", $("#url").val() + "/exchange", true);
+    xhr.withCredentials = true;
+    xhr.onreadystatechange = () => {
+        // Call a function when the state changes.
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            // Request finished. Do processing here.
+            console.log("Success");
             window.open($("#url").val() + "/restricted",'_blank').focus();
-          })
-            .fail(function( jqXHR, textStatus, errorThrown ) {
-                console.log("jqXHR", jqXHR);
-                console.log("textStatus", textStatus);
-                console.log("errorThrown", errorThrown);
-                alert( "error: " + textStatus );
-            });
-    } else {
-        window.open($("#url").val() + "/restricted",'_blank').focus();
-    }
+        }
+    };
+
+    xhr.send();
+
+    // if($("#isExchange").is(":checked")) {
+    //     var jqxhr = $.get( $("#url").val() + "/exchange", function(data) {
+    //         window.open($("#url").val() + "/restricted",'_blank').focus();
+    //       })
+    //         .fail(function( jqXHR, textStatus, errorThrown ) {
+    //             console.log("jqXHR", jqXHR);
+    //             console.log("textStatus", textStatus);
+    //             console.log("errorThrown", errorThrown);
+    //             alert( "error: " + textStatus );
+    //         });
+    // } else {
+    //     window.open($("#url").val() + "/restricted",'_blank').focus();
+    // }
 }
